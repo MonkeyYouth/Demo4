c9df8bdf401c4c086a6815ddb2484c35922f222b
MonkeyYouth <yang.zhang@ifacilitylink.com>
2017-03-25 09:01:08 +0800

    add XCodeProject

---
+++ Demo/Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by Mac on 17/3/25.
+//  Copyright © 2017年 MTV_Youth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo/Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by Mac on 17/3/25.
+//  Copyright © 2017年 MTV_Youth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
